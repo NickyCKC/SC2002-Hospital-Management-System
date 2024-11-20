686073adc09fe7b98542dec7d9c3b606aafa0a43
Li Liyi <116091440+liliyigz@users.noreply.github.com>
2024-11-20 09:18:46 +0800

    Add files via upload

---
+++ HospitalManagementSystem/HospitalManagementSystem/src/main/java/constants/FilePath.java
@@ -0,0 +1,14 @@
+package constants;
+
+public final class FilePath {
+	private FilePath() { } // No need to instantiate the class, we can hide its constructor
+
+	// Update to file path of people using
+	public static final String ACCOUNT_FILE_PATH = "C:\\NTU\\Y2S1\\SC2002\\HospitalManagementSystem_Final\\HospitalManagementSystem\\src\\main\\resources\\Accounts.xlsx";
+	public static final String STAFF_INFO_FILE_PATH = "C:\\NTU\\Y2S1\\SC2002\\HospitalManagementSystem_Final\\HospitalManagementSystem\\src\\main\\resources\\Staff_Info.xlsx";
+	public static final String PATIENT_INFO_FILE_PATH = "C:\\NTU\\Y2S1\\SC2002\\HospitalManagementSystem_Final\\HospitalManagementSystem\\src\\main\\resources\\Patient_Info.xlsx";
+	public static final String MEDICAL_RECORDS_FILE_PATH = "C:\\NTU\\Y2S1\\SC2002\\HospitalManagementSystem_Final\\HospitalManagementSystem\\src\\main\\resources\\Medical_Records.xlsx";
+	public static final String APPOINTMENTS_FILE_PATH = "C:\\NTU\\Y2S1\\SC2002\\HospitalManagementSystem_Final\\HospitalManagementSystem\\src\\main\\resources\\Appointments.xlsx";
+	public static final String MEDICAL_INVENTORY_FILE_PATH = "C:\\NTU\\Y2S1\\SC2002\\HospitalManagementSystem_Final\\HospitalManagementSystem\\src\\main\\resources\\Medicine_List.xlsx";
+
+}
